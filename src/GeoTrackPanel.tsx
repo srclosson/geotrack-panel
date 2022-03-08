c29f191d7bb9759c24581f8954b795f196391f13
Stephanie Closson <srclosson@gmail.com>
2022-03-08 13:09:20 -0400

    displaying lineLayer

--- src/GeoTrackPanel.tsx
+++ src/GeoTrackPanel.tsx
@@ -13,64 +13,63 @@ import { TerrainLayer } from '@deck.gl/geo-layers';
 // import { Position } from 'deck.gl';
 //import { DataFrame } from '@grafana/data';
 
-
 const MAX_ZOOM = 19;
 const MIN_ZOOM = 2;
 
-const MAPBOX_TOKEN="pk.eyJ1Ijoic3JjbG9zc29uIiwiYSI6ImNsMGdxamNidDAxOTcza21qOTFwNmRkNGkifQ.u-AcyC1afRBc-Eb-dOZ0iw"
+const MAPBOX_TOKEN = 'pk.eyJ1Ijoic3JjbG9zc29uIiwiYSI6ImNsMGdxamNidDAxOTcza21qOTFwNmRkNGkifQ.u-AcyC1afRBc-Eb-dOZ0iw';
 
 const BASEMAP_TILE_SOURCE_NAME = 'simple-tiles';
 const BASEMAP_TILE_SERVERS = [
-    // 'http://a.tile.osm.org/{z}/{x}/{y}.png',
-    // 'http://b.tile.osm.org/{z}/{x}/{y}.png',
-    // 'http://c.tile.osm.org/{z}/{x}/{y}.png',
-    '//stamen-tiles-a.a.ssl.fastly.net/terrain/{z}/{x}/{y}.jpg',
-    '//stamen-tiles-b.a.ssl.fastly.net/terrain/{z}/{x}/{y}.jpg',
-    '//stamen-tiles-c.a.ssl.fastly.net/terrain/{z}/{x}/{y}.jpg',
+  // 'http://a.tile.osm.org/{z}/{x}/{y}.png',
+  // 'http://b.tile.osm.org/{z}/{x}/{y}.png',
+  // 'http://c.tile.osm.org/{z}/{x}/{y}.png',
+  '//stamen-tiles-a.a.ssl.fastly.net/terrain/{z}/{x}/{y}.jpg',
+  '//stamen-tiles-b.a.ssl.fastly.net/terrain/{z}/{x}/{y}.jpg',
+  '//stamen-tiles-c.a.ssl.fastly.net/terrain/{z}/{x}/{y}.jpg',
 ];
 const BASEMAP_ATTRIBUTION = `Map tiles by <a href="http://stamen.com">Stamen Design</a>, under
 <a href="http://creativecommons.org/licenses/by/3.0"> CC BY 3.0</a>. Data by
 <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">
 ODbL</a>.`.replace(/\n/gm, '');
 const COMMON_LAYER_CONFIG = {
-    minZoom: 2,
-    maxZoom: 17, // New data will be requested until this level
-    pixelScaleFactor: 8,
-    tileSize: 256,
-    isTms: true,
-    topoLayerClusteringSwithLevel: 13,
-    maxVisibleRasterLayers: 3,
-    maxConfigurableLayers: 26,
+  minZoom: 2,
+  maxZoom: 17, // New data will be requested until this level
+  pixelScaleFactor: 8,
+  tileSize: 256,
+  isTms: true,
+  topoLayerClusteringSwithLevel: 13,
+  maxVisibleRasterLayers: 3,
+  maxConfigurableLayers: 26,
 };
 const MAP_CONFIG = {
-    MIN_ZOOM: 1,
-    MAX_ZOOM: 18,
-    INITIAL_ZOOM: 9,
-    SHOW_TILE_BOUNDARIES: false,
-    DRAG_ROTATE: false,
-    ZOOM_NO_DATA: 2,
-    SEARCH_DEFAULT_ZOOM: 14,
+  MIN_ZOOM: 1,
+  MAX_ZOOM: 18,
+  INITIAL_ZOOM: 9,
+  SHOW_TILE_BOUNDARIES: false,
+  DRAG_ROTATE: false,
+  ZOOM_NO_DATA: 2,
+  SEARCH_DEFAULT_ZOOM: 14,
 };
 
 const MAPBOX_BASE_LAYER = {
-    version: 8,
-    sources: {
-        [BASEMAP_TILE_SOURCE_NAME]: {
-            type: 'raster',
-            tiles: BASEMAP_TILE_SERVERS,
-            tileSize: COMMON_LAYER_CONFIG.tileSize,
-            attribution: BASEMAP_ATTRIBUTION,
-        }
+  version: 8,
+  sources: {
+    [BASEMAP_TILE_SOURCE_NAME]: {
+      type: 'raster',
+      tiles: BASEMAP_TILE_SERVERS,
+      tileSize: COMMON_LAYER_CONFIG.tileSize,
+      attribution: BASEMAP_ATTRIBUTION,
     },
-    layers: [
-        {
-            id: BASEMAP_TILE_SOURCE_NAME,
-            type: 'raster',
-            source: BASEMAP_TILE_SOURCE_NAME,
-            minzoom: MAP_CONFIG.MIN_ZOOM,
-            maxzoom: MAP_CONFIG.MAX_ZOOM,
-        }
-    ],
+  },
+  layers: [
+    {
+      id: BASEMAP_TILE_SOURCE_NAME,
+      type: 'raster',
+      source: BASEMAP_TILE_SOURCE_NAME,
+      minzoom: MAP_CONFIG.MIN_ZOOM,
+      maxzoom: MAP_CONFIG.MAX_ZOOM,
+    },
+  ],
 };
 
 interface Props extends PanelProps<SimpleOptions> {}
@@ -90,14 +89,14 @@ export const GeotrackPanel: React.FC<Props> = ({ options, data, width, height })
     bearing: 0,
     pitch: 0,
     maxZoom: MAX_ZOOM,
-    minZoom: MIN_ZOOM
+    minZoom: MIN_ZOOM,
   };
 
   const ELEVATION_DECODER = {
     rScaler: 6553.6,
     gScaler: 25.6,
     bScaler: 0.1,
-    offset: -10000
+    offset: -10000,
   };
 
   const terrainLayer = new TerrainLayer({
@@ -108,7 +107,7 @@ export const GeotrackPanel: React.FC<Props> = ({ options, data, width, height })
     elevationData: TERRAIN_IMAGE,
     texture: SURFACE_IMAGE,
     wireframe: false,
-    color: [255, 255, 255]
+    color: [255, 255, 255],
   });
 
   // const hexLayer = new HexagonLayer({
@@ -119,11 +118,10 @@ export const GeotrackPanel: React.FC<Props> = ({ options, data, width, height })
   //   radius: 200,
   //   elevationScale: 4,
   //   getPosition: d => {
-      
+
   //   }
   // });
 
-    
   const lat = data.series[0].fields[1].values.toArray();
   const latTime = data.series[0].fields[0].values.toArray();
   const lon = data.series[1].fields[1].values.toArray();
@@ -132,22 +130,22 @@ export const GeotrackPanel: React.FC<Props> = ({ options, data, width, height })
   let lineLayerData = [];
   for (let i = 1; i <= lat.length - 1; i++) {
     lineLayerData.push({
-      inbound: i-1,
+      inbound: i - 1,
       outbound: i,
       from: {
-        name: `lat: ${lat[i-1]}, lon: ${lon[i-1]}, ele: ${ele[i-1]}, time: ${latTime[i-1]}`,
-        coordinates: [lat[i-1], lon[i-1], ele[i-1]],
-        hr: hr[i-1]
+        name: `lat: ${lat[i - 1]}, lon: ${lon[i - 1]}, ele: ${ele[i - 1]}, time: ${latTime[i - 1]}`,
+        coordinates: [lat[i - 1], lon[i - 1], ele[i - 1]],
+        hr: hr[i - 1],
       },
       to: {
         name: `lat: ${lat[i]}, lon: ${lon[i]}, ele: ${ele[i]} time: ${latTime[i]}`,
         coordinates: [lat[i], lon[i], ele[i]],
-        hr: hr[i]
-      }
+        hr: hr[i],
+      },
     });
   }
-    
-  console.log("linelayerdata", lineLayerData);
+
+  console.log('linelayerdata', lineLayerData);
   const lineLayer = new LineLayer({
     id: 'line-layer',
     data: lineLayerData,
@@ -156,20 +154,20 @@ export const GeotrackPanel: React.FC<Props> = ({ options, data, width, height })
     getSourcePosition: (d: any) => d.from.coordinates,
     getTargetPosition: (d: any) => d.to.coordinates,
     getColor: (d: any) => {
-      const result: RGBAColor = [((d.from.hr-140)/(190-140))*255, 140, 0];
-      console.log("colour", d, result);
+      const result: RGBAColor = [((d.from.hr - 140) / (190 - 140)) * 255, 140, 0];
+      console.log('colour', d, result);
       return result;
     },
   });
 
   return (
     <div>
-      <DeckGL initialViewState={INITIAL_VIEW_STATE} controller={true} layers={[terrainLayer, lineLayer]} >
+      <DeckGL initialViewState={INITIAL_VIEW_STATE} controller={true} layers={[terrainLayer, lineLayer]}>
         <StaticMap
           mapboxApiAccessToken={MAPBOX_TOKEN}
           //mapStyle={BASEMAP.POSITRON}
           mapStyle={MAPBOX_BASE_LAYER}
-        />        
+        />
       </DeckGL>
     </div>
   );
