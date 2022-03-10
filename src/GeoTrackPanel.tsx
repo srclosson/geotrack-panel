ee340dd52d8a598c351d2cff3f4f586a98f4d3cf
Stephanie Closson <srclosson@gmail.com>
2022-03-10 19:21:23 -0400

    added streaming and icon layer

--- src/GeoTrackPanel.tsx
+++ src/GeoTrackPanel.tsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
-import { PanelProps, DataFrame } from '@grafana/data';
+import { PanelProps, PanelData, LoadingState } from '@grafana/data';
 import { Options } from 'types';
 import { css, cx } from 'emotion';
 import DeckGL from '@deck.gl/react';
-import { LineLayer } from '@deck.gl/layers';
-import {CesiumIonLoader} from '@loaders.gl/3d-tiles';
+import { IconLayer, LineLayer } from '@deck.gl/layers';
+import { CesiumIonLoader } from '@loaders.gl/3d-tiles';
 
 //import { BASEMAP } from '@deck.gl/carto';
 import StaticMap from 'react-map-gl';
@@ -16,43 +16,57 @@ import { MapControls } from './components/MapControls';
 
 interface Props extends PanelProps<Options> {}
 
-function readTimePosData(series: DataFrame[], l: any): any[] {
+function readTimePosData(data: PanelData, l: any): any[] {
   const lineLayerData: any[] = [];
 
   var lat: number[] = [];
   // var latTime: number[] = [];
   var lon: number[] = [];
   var ele: number[] = [];
+  const series = data.series;
 
   // ugly but functional
   for (var s of series) {
     // console.log(s);
-    if (lat.length == 0) { lat = s.fields.find((field) => field.name === l.dataMapping.latitude)?.values.toArray() || [] };
-    if (lat.length == 0) { 
+    if (lat.length === 0) {
+      lat = s.fields.find((field) => field.name === l.dataMapping.latitude)?.values.toArray() || [];
+    }
+    if (lat.length === 0) {
       if (s.name === l.dataMapping.latitude) {
-        lat = s.fields[1].values.toArray()
+        lat = s.fields[1].values.toArray();
       }
     }
 
-    if (lon.length == 0) { lon = s.fields.find((field) => field.name === l.dataMapping.longitude)?.values.toArray() || [] };
-    if (lon.length == 0) { 
+    if (lon.length === 0) {
+      lon = s.fields.find((field) => field.name === l.dataMapping.longitude)?.values.toArray() || [];
+    }
+    if (lon.length === 0) {
       if (s.name === l.dataMapping.longitude) {
-        lon = s.fields[1].values.toArray()
+        lon = s.fields[1].values.toArray();
       }
     }
-    
-    if (ele.length == 0) { ele = s.fields.find((field) => field.name === l.dataMapping.elevation)?.values.toArray() || [] };
-    if (ele.length == 0) { 
-      if (s.name === l.dataMapping.elevation) {
-        ele = s.fields[1].values.toArray()
-      }
-    }
-  };
 
-//latTime.length == 0 ||
-  if (lat.length == 0 || lon.length == 0 || ele.length == 0) {
-    console.log("missing required fields");
+    if (ele.length === 0) {
+      ele = s.fields.find((field) => field.name === l.dataMapping.elevation)?.values.toArray() || [];
+    }
+    if (ele.length === 0) {
+      if (s.name === l.dataMapping.elevation) {
+        ele = s.fields[1].values.toArray();
+      }
+    }
+  }
+
+  if (lat.length === 0 || lon.length === 0 || ele.length === 0) {
+    console.log('missing required fields');
     return [];
+  }
+
+  if (data.state === LoadingState.Streaming) {
+    return [[
+      lon[lon.length - 1],
+      lat[lat.length - 1],
+      ele[ele.length - 1] + l.dataMapping.elevationOffset,
+    ]]
   }
 
   for (let i = 1; i <= lat.length - 1; i++) {
@@ -65,7 +79,7 @@ function readTimePosData(series: DataFrame[], l: any): any[] {
       },
       to: {
         name: `lon: ${lon[i]}, lat: ${lat[i]}, ele: ${ele[i]}`,
-        coordinates: [lon[i], lat[i], ele[i] + 5],
+        coordinates: [lon[i], lat[i], ele[i] + l.dataMapping.elevationOffset],
       },
     });
   }
@@ -78,7 +92,7 @@ export const GeotrackPanel: React.FC<Props> = ({ options, data, width, height })
   const styles = getStyles();
   const config: any = parseConfigJson(options.editor.configJson);
   const [showTerrainLayer, setShowTerrainLayer] = useState(true);
-  // console.log(`🚀 ~ config`, config);
+  console.log(`🚀 ~ data`, data);
 
   let initialLat: number | undefined = undefined;
   let initialLon: number | undefined = undefined;
@@ -98,14 +112,42 @@ export const GeotrackPanel: React.FC<Props> = ({ options, data, width, height })
           loadOptions: l.loadOptions,
           // override scenegraph subLayer prop
           _subLayerProps: {
-            scenegraph: {_lighting: 'flat'}
-          }
+            scenegraph: { _lighting: 'flat' },
+          },
+        });
+      case 'IconLayer':
+        const iconLayerData = readTimePosData(data, l);
+        console.log("iconLayerData", iconLayerData);
+        initialLon = iconLayerData[0][0];
+        initialLat = iconLayerData[0][1];
+        return new IconLayer({
+          id: l.id,
+          data: iconLayerData,
+          pickable: l.pickable,
+          // iconAtlas and iconMapping are required
+          // getIcon: return a string
+          iconAtlas: l.iconAtlas, //'https://raw.githubusercontent.com/visgl/deck.gl-data/master/website/icon-atlas.png',
+          iconMapping: {
+            marker: { x: 0, y: 0, width: 128, height: 128, mask: true },
+          },
+          getIcon: (d) => l.icon,
+          sizeScale: l.sizeScale,
+          getPosition: (d: any) => {
+            console.log("getPosition in IconLayer", d);
+            return d;
+          },
+          getSize: (d: any) => l.size,
+          getColor: (d: any) => l.color,
         });
       case 'LineLayer':
-        const lineLayerData = readTimePosData(data.series, l);
+        const lineLayerData = readTimePosData(data, l);
 
-        if (!initialLon && lineLayerData.length > 0) initialLon = lineLayerData[lineLayerData.length-1].to.coordinates[0];
-        if (!initialLat && lineLayerData.length > 0) initialLat = lineLayerData[lineLayerData.length-1].to.coordinates[1];
+        if (!initialLon && lineLayerData.length > 0) {
+          initialLon = lineLayerData[lineLayerData.length - 1].to.coordinates[0];
+        }
+        if (!initialLat && lineLayerData.length > 0) {
+          initialLat = lineLayerData[lineLayerData.length - 1].to.coordinates[1];
+        }
 
         const ll = new LineLayer({
           id: l.id,
